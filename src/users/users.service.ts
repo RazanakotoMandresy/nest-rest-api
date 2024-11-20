581f20cb5177a0a387c21d9d3224d4be8fceac03
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-20 20:44:10 +0300

    basic services classes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -66,4 +66,25 @@ export class UsersService {
     this.users.push(newUser);
     return newUser;
   }
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'ADMIN' | 'ENGINNEER' | 'INTERN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
 }
