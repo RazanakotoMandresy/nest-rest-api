eca1693fa906f5ab8fcdf49de7251fc46329648e
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-20 14:29:16 +0300

    implement services findll until create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'name1',
+      email: 'mail@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'name2',
+      email: 'mail2@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'name3',
+      email: 'mail3@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'name4',
+      email: 'mail4@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'name5',
+      email: 'mail5@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 6,
+      name: 'name6',
+      email: 'mail6@gmail.com',
+      role: 'ENGINEER',
+    },
+  ];
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+  create(user: {
+    name: string;
+    email: string;
+    role: 'ADMIN' | 'ENGINNEER' | 'INTERN';
+  }) {
+    // > [11,2,22,1].sort((a, b) => a - b)
+    // [ 1, 2, 11, 22 ]
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
