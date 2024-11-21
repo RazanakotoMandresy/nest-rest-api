c3b51f1bddf147bf61a4ce4ebe4a5c0bb0be23f5
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-11-21 07:38:38 +0300

    implemented userServices in the controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,27 +8,45 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
   @Get() //Get /users or get with some query
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.userService.findAll(role);
   }
   @Get(':id') // Get /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    // +id convert the id into number type it's called unary
+    return this.userService.findOne(+id);
   }
   @Post() // Post /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'ADMIN' | 'ENGINNEER' | 'INTERN';
+    },
+  ) {
+    return this.userService.create(user);
   }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUser: {}) {
-    return { id, ...updateUser };
+  update(
+    @Param('id') id: string,
+    @Body()
+    updateUser: {
+      name?: string;
+      email?: string;
+      role?: 'ADMIN' | 'ENGINNEER' | 'INTERN';
+    },
+  ) {
+    return this.userService.update(+id, updateUser);
   }
   @Delete(':id')
   deleteOne(@Param('id') id: string) {
-    return { id };
+    return this.userService.delete(+id);
   }
 }
